d9497d9a63120c6737220a5c87bc5bac93fc479e
hekmat <hak68@mail.aub.edu>
2023-05-04 18:27:57 +0300

    added address to user model

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -14,6 +14,10 @@ const userSchema = new Schema(
       trim: true,
       match: /^\S+@\S+\.\S+$/,
     },
+    address: {
+      type: String,
+      required: true,
+    },
     password: {
       type: String,
       required: true,
