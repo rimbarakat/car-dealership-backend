a6a12656db60d49a2736ffea587cb608fc2e6c21
kouk34 <114072794+KarimKaraki@users.noreply.github.com>
2023-05-06 15:20:37 +0300

    Implemented a rate limit on the login tries, still need to be moved to a new file but funtionality works

---
+++ src/middlewares/rateLimit.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const rateLimit = require("express-rate-limit");
+
+const app = express();
+const HttpError = require("../http-errors/HttpError");
+
+const login_limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 5, // Limit each IP to 100 requests per windowMs
+    message: "Too many login tries, please try again later.",
+});
+
+module.exports = login_limiter;
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,10 +2,18 @@ const express = require("express");
 
 const controller = require("../controllers/authController");
 const authMiddleware = require("../middlewares/authMiddleware");
+//const login_limiter = require("../middlewares/login_limiter");
 const router = express.Router();
+const rateLimit = require("express-rate-limit");
+
+const login_limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 3, // Limit each IP to 100 requests per windowMs
+    message: "Too many login tries, please try again later.",
+});
 
 router.get("/", authMiddleware, controller.authenticate);
-router.post("/login", controller.loginUser);
+router.post("/login", login_limiter, controller.loginUser); // Apply the login_limiter middleware to the /login route
 router.post("/register", controller.registerUser);
 
 module.exports = router;
